e809fe262aaa39bf877ecb89c2e5bd064a12416b
Kirisaki <kirisaki124@yahoo.com>
2020-01-01 12:40:26 +0700

    Update: Check win row and column

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -75,30 +76,144 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonClick(View view){
         int id = view.getId();
+        boolean isWin = false;
 
-        if(playerTurn) { // Player x
+        mapButton(playerTurn, id);
+        if(playerTurn) { // Player X
             ((Button) findViewById(view.getId())).setText("X");
+            isWin = checkWin(gameSize, "X", id);
             playerTurn = false;
         }
-        else { // Player o
+        else { // Player O
             ((Button) findViewById(view.getId())).setText("O");
+            isWin = checkWin(gameSize,"O", id);
             playerTurn = true;
         }
 
-        mapButton(playerTurn, id);
 
         this.logMap();
+        if(isWin) {
+            if (!playerTurn) Toast.makeText(getApplicationContext(), "X win", Toast.LENGTH_LONG).show();
+            else Toast.makeText(getApplicationContext(), "O win", Toast.LENGTH_LONG).show();
+        }
+    }
+    private int getBtnX(int btnId) {
+        int count = 0;
+        for (int i = 0; i < gameSize; i++) {
+            for (int j = 0; j < gameSize; j++) {
+                if (count == btnId) return i;
+                count++;
+            }
+        }
+        return 0;
     }
 
-    private boolean checkWin(int x, int y){
-        
-        return true;
+    private int getBtnY(int btnId) {
+        int count = 0;
+        for (int i = 0; i < gameSize; i++) {
+            for (int j = 0; j < gameSize; j++) {
+                if (count == btnId) return j;
+                count++;
+            }
+        }
+        return 0;
+    }
+
+    private boolean checkWinRow(int gameSize, String playerTurn) {
+
+        for (int j = 0; j < gameSize; j++) {
+            boolean win = true;
+            for (int i = 0; i < gameSize; i++) {
+                if (!game[j][i].equalsIgnoreCase(playerTurn)) {
+                    win = false;
+                }
+            }
+            if (win) return win;
+        }
+        return false;
+    }
+
+    private boolean checkWinColumn(int gameSize, String playerTurn) {
+        for (int j = 0; j < gameSize; j++) {
+            boolean win = true;
+            for (int i = 0; i < gameSize; i++) {
+                if (!game[i][j].equalsIgnoreCase(playerTurn)) {
+                    win = false;
+                }
+            }
+            if (win) return win;
+
+        }
+        return false;
+    }
+
+//    private boolean checkWinDiagonalLeftToRight(int gameSize, int x, int y){
+//        boolean win = true;
+//        for (int i = 0; i < gameSize; i++) {
+//            try {
+//                if (!game[x][y].equalsIgnoreCase(game[x + i][y + i])) {
+//                    win = false;
+//                }
+//            }catch (Exception e) {
+//                continue;
+//            }
+//        }
+//        if (win) {
+//            return win;
+//        }
+//
+//        return false;
+//    }
+//
+//    private boolean checkWinDiagonalRightToLeft(int gameSize, int x, int y){
+//        boolean win = true;
+//        for (int i = gameSize; i < 0; i--) {
+//            try {
+//                if (!game[x][y].equalsIgnoreCase(game[x + i][y + i])) {
+//                    win = false;
+//                }
+//            }catch (Exception e) {
+//                continue;
+//            }
+//        }
+//        if (win) {
+//            return win;
+//        }
+//
+//        return false;
+//    }
+
+    private boolean checkWin(int gameSize, String playerTurn, int btnId){
+
+//        Check row
+        if(this.checkWinRow(gameSize, playerTurn)){
+            return true;
+        }
+//        Check column
+        if(this.checkWinColumn(gameSize, playerTurn)){
+            return true;
+        }
+
+//        int x = getBtnX(btnId);
+//        int y = getBtnY(btnId);
+
+////        Check diagonal left to right
+//        if(this.checkWinDiagonalLeftToRight(gameSize, x, y)) {
+//            return true;
+//        }
+//
+////        Check diagonal right to left
+//        if(this.checkWinDiagonalRightToLeft(gameSize, x, y)) {
+//            return true;
+//        }
+        return false;
     }
 
     private void createGame() {
 
         gameSize = Integer.parseInt(((EditText)findViewById(R.id.gameSize)).getText().toString());
         ((TableLayout) findViewById(R.id.mainGame)).removeAllViews();
+
 //        Create new game
         int countId = 0;
 
